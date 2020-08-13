5c4658592c02020a37377ed4d4105e84797ba4ff
Ken Huang <khuang.dev@gmail.com>
2020-08-13 02:04:18 -0700

    Material ui dependencies

---
+++ src/theme/index.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: "#ffffff",
+    },
+  },
+  fonts: {
+    primary: {
+      main: "Raleway, sans-serif",
+    },
+    secondary: {
+      main: "Homemade Apple, cursive",
+    },
+  },
+});
