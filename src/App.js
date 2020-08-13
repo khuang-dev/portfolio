77a7d3585e8f3100665190fbdbd5e80fcc8640b8
Ken Huang <khuang.dev@gmail.com>
2020-08-13 02:05:34 -0700

    Remove react-app defaults

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "./theme";
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Banner />
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
